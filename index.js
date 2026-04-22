3efe54926ebbe86f770f3d42717768768bc91677
karingawairimu304-dot <karingawairimu304@gmail.com>
2026-04-22 09:35:35 +0300

    Updated index.js
    Full backend API for 7 Gates real estate platform including Admin systems, agents, customer, property managements, authentication and payment endpoints. Built with Node,js and Express.

--- index.js
+++ index.js
@@ -277,6 +277,8 @@ app.post("/pay", auth, (req, res) => {
 });
 
 // ================= START SERVER =================
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("🔥 7Gates FULL REAL ESTATE SYSTEM RUNNING");
 });
